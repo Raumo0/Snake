0e9a2c04b0424283c46cc73ab654e2dbe3eda859
Daniil <RaumoO@informatics.by>
2018-10-31 04:11:53 +0300

    Added a rectangle vicious world

---
+++ Assets/Scripts/GameMain.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class GameMain{
+    public int ScreenTop { get; private set; }
+    public int ScreenBottom { get; private set; }
+    public int ScreenLeft { get; private set; }
+    public int ScreenRight { get; private set; }
+
+    private GameMain()
+    {
+        Camera camera = Camera.main;
+        ScreenBottom = 0;
+        ScreenLeft = 0;
+        ScreenTop = camera.pixelHeight;
+        ScreenRight = camera.pixelWidth;
+    }
+
+    public static GameMain GetInstance()
+    {
+        return SingletonHolder.INSTANCE;
+    }
+
+    private static class SingletonHolder
+    {
+        public static readonly GameMain INSTANCE = new GameMain();
+    }
+}
---
+++ Assets/Scripts/GameScripts/Actors/ViciousWorld.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class ViciousWorld : MonoBehaviour {
+    private Rigidbody2D rgb2d;
+    private Vector2 point;
+
+	void Awake () {
+        rgb2d = GetComponent<Rigidbody2D>();
+    }
+
+    public void FixedUpdate()
+    {
+        point = rgb2d.position;
+        TransferFromBorders();
+        rgb2d.position = point;
+    }
+
+    private void TransferFromBorders()
+    {
+        GameMain gameMain = GameMain.GetInstance();
+        if (point.x > gameMain.ScreenRight)
+            point.x = gameMain.ScreenLeft;
+        else if (point.x < gameMain.ScreenLeft)
+            point.x = gameMain.ScreenRight;
+        if (point.y > gameMain.ScreenTop)
+            point.y = gameMain.ScreenBottom;
+        else if (point.y < gameMain.ScreenBottom)
+            point.y = gameMain.ScreenTop;
+    }
+}
